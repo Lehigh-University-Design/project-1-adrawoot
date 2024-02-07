897574578af886ba631b2b288820699250f52b5c
“adrawoot” <adrawoot@gmail.com>
2024-02-07 08:00:24 -0500

    final

--- js/navigator.js
+++ js/navigator.js
@@ -2,10 +2,19 @@
  *  Websites list in order
  */
 var websites = [
-    "1.html",
-    "2.html",
-    "3.html",
-    "4.html"
+    "https://lehigh-university-design.github.io/project-1-awb324/",
+    "https://lehigh-university-design.github.io/project-1-sit225/",
+    "https://lehigh-university-design.github.io/project-1-lar326/",
+    "https://lehigh-university-design.github.io/project-1-adrawoot/",
+    "https://lehigh-university-design.github.io/project-1-mmurata22/",
+    "https://lehigh-university-design.github.io/project-1-SSShee77y/",
+    "https://lehigh-university-design.github.io/project-1-eyt225/",
+    "https://lehigh-university-design.github.io/project-1-shirls326/",
+    "https://lehigh-university-design.github.io/project-1-Colin941/",
+    "https://lehigh-university-design.github.io/project-1-ellafall/",
+    "https://lehigh-university-design.github.io/project-1-laurj46/",
+    "https://lehigh-university-design.github.io/project-1-kaydolan/",
+    "https://lehigh-university-design.github.io/project-1-dsw225/"
 ];
 
 /*
@@ -14,8 +23,9 @@ var websites = [
 var currentIndex = 0;
 
 window.onload = function getCurrentIndex() {
-    var path = window.location.href; 
-    var page = path.split("/");
+    var path = window.location.href.split("#")[0];
+    currentIndex = websites.indexOf(path);
+    
 
     if (page.includes("https") || page.includes("http"))
         currentIndex = websites.indexOf(path);
